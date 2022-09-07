c529cf51cebf3af1aa4298a558113e0775d8ef8c
고경훈 <sand9688@naver.com>
2022-09-07 15:02:28 +0900

    조건문

--- 03.javaScript/ch03.조건문/03.ampm.js
+++ 03.javaScript/ch03.조건문/03.ampm.js
@@ -6,3 +6,10 @@
  } else{
     console.log(`${currentTime.toLocaleString()}는 오후입니다.\n`)
  }
+
+ //조건 연산자 ,3항 연산자
+ const ampm= currentTime.getHours() <12 ? '오전 ' : '오후' ;
+ console.log(`${currentTime.toLocaleString()}sms ${ampm}입니다.\n`)
+
+ // 파이썬 :(a = 값1 if 조건 else 값2)  ==> JS (a = 조건 ? 값1 : 값2)
+ 
