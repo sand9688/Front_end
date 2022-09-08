838f51b3f4b59f9de89b45a64ed8a1bc8c0601cf
고경훈 <sand9688@naver.com>
2022-09-08 11:49:12 +0900

    반복문_연습

---
+++ 03.javaScript/ch04.반복문/05.enhansceFor.js
@@ -0,0 +1,11 @@
+const array=[52,273,568,166,1652]
+
+//for ~of
+for (let item of array){
+    console.log(item, typeof(item));
+}
+
+//for ~ in, 
+for (let i in array){
+    console.log(i, array[i]);
+}
---
+++ 03.javaScript/ch04.반복문/07.피타고라스.js
@@ -0,0 +1,11 @@
+//a+b+c = 1000을 만족하는 피타골스 수
+// 단, a<b<c 
+for (let b = 1; b < 1000; b++){
+    for (let a = 1; a<b; a++){
+        c = 1000 - a - b;
+        if (a*a + b*b == c*c){
+            console.log(a,b,c);
+            break;
+        }
+    }
+}
